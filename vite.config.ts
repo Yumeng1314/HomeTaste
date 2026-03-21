eea9242957a9ca62a07b9b4f19c164f12f4f21b4
Yumeng1314 <yluo41@ur.rochester.edu>
2026-03-21 00:15:50 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",         // Vercel 必须这样
+  base: '/HomeTaste/',
   build: {
-    outDir: "dist",  // 你原来就是 dist，保留
+    outDir: 'dist',
   },
 });
